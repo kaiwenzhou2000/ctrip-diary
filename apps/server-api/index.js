da79f5f041c1b69b6298e6f29ba7520f1b7995db
周凯文 <kaiwen.zhou@mail.ecust.edu.cn>
2024-03-30 20:58:02 +0800

    feat: 添加server api

---
+++ apps/server-api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+// 当对主页发出 GET 请求时，响应“hello world”
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+// 监听 3000 端口
+app.listen(3000, function() {
+  console.log('app is listening at port 3000');
+})
